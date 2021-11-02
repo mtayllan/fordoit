db98eab4e63f3e57fea83721866cfd4e7a0bf689
mtayllan <messiastayllan@gmail.com>
2021-11-02 10:42:32 -0300

    ✨ Add auth0 authentication routes

---
+++ app/controllers/auth0_controller.rb
@@ -0,0 +1,28 @@
+class Auth0Controller < ApplicationController
+  def callback
+    auth_info = request.env["omniauth.auth"]
+    session[:userinfo] = auth_info["extra"]["raw_info"]
+
+    redirect_to "/"
+  end
+
+  def failure
+    @error_msg = request.params["message"]
+  end
+
+  def logout
+    reset_session
+    redirect_to logout_url
+  end
+
+  private
+
+  def logout_url
+    request_params = ActionController::Parameters.new({
+      returnTo: root_url,
+      client_id: ENV["AUTH0_CLIENT_ID"]
+    }).permit(:returnTo, :client_id)
+
+    URI::HTTPS.build(host: ENV["AUTH0_DOMAIN"], path: "/v2/logout", query: request_params.to_query).to_s
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
+  get "/auth/auth0/callback" => "auth0#callback"
+  get "/auth/failure" => "auth0#failure"
+  get "/auth/logout" => "auth0#logout"
+
+  get "login", to: "login#index"
+
   resources :tasks, only: [:create, :update]
   resources :in_backlog_tasks, only: [:create, :update, :destroy]
   resources :events, only: [:create, :update, :destroy]
