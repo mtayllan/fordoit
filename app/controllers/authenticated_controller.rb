1d040d0e6b777c062c5c84639c2d066b2d69713a
mtayllan <messiastayllan@gmail.com>
2021-11-02 11:02:46 -0300

    ✨ Add current_user to authenticated controller

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -6,4 +6,12 @@ class AuthenticatedController < ApplicationController
   def logged_in_using_omniauth?
     redirect_to login_path unless session[:userinfo].present?
   end
+
+  def current_user
+    User.find_by(auth0_id: auth0_id)
+  end
+
+  def auth0_id
+    session[:userinfo]["sub"].split("|")[1]
+  end
 end
