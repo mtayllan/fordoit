da1083aa8f8377780c0a1e706b7af753413520bc
mtayllan <messiastayllan@gmail.com>
2021-11-03 19:10:06 -0300

    ✨ Order events by datetime

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < AuthenticatedController
   def index
     @tasks = current_user.tasks.pending
     @backlog_tasks = current_user.tasks.in_backlog
-    @events = current_user.events.not_done
+    @events = current_user.events.not_done.order(datetime: :asc)
     @repeats = current_user.repeats.where("days @> ARRAY[?]::integer[]", [Date.today.wday])
 
     @task = Task.new
