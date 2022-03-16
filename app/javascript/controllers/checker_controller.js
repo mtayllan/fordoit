16e479287d604560d91c0b829b4a71ae8f7ba5ff
mtayllan <messiastayllan@gmail.com>
2022-03-16 08:24:14 -0300

    🐛 Fix not being possible to check tasks

--- app/javascript/controllers/checker_controller.js
+++ app/javascript/controllers/checker_controller.js
@@ -2,6 +2,6 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   submitAction () {
-    this.element.nextElementSibling.querySelector('input[type=submit]').click()
+    this.element.nextElementSibling.querySelector('button[type=submit]').click()
   }
 }
