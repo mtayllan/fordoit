105427bbb231260eae51c22c85809bd276757b15
mtayllan <messiastayllan@gmail.com>
2022-04-13 21:13:13 -0300

    🗃️ Add timezone config to user table

---
+++ db/migrate/20220413231724_add_time_zone_to_user.rb
@@ -0,0 +1,5 @@
+class AddTimeZoneToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :timezone, :string, default: "UTC"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2021_11_03_223133) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_04_13_231724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "title", null: false
-    t.datetime "datetime", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "datetime", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.datetime "done_at"
+    t.datetime "done_at", precision: nil
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -29,26 +28,27 @@ ActiveRecord::Schema[6.1].define(version: 2021_11_03_223133) do
     t.string "title", null: false
     t.integer "days", default: [], array: true
     t.date "hits", default: [], array: true
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_repeats_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
     t.integer "status", default: 0, null: false
-    t.datetime "ended_at"
+    t.datetime "ended_at", precision: nil
     t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "auth0_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "timezone", default: "UTC"
     t.index ["auth0_id"], name: "index_users_on_auth0_id", unique: true
   end
 
