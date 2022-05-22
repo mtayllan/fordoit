0dc082b1e795d19aae7f4658e898dbd5d7f2ff54
mtayllan <messiastayllan@gmail.com>
2022-05-22 07:43:36 -0300

    💄 Set font-family as Lato

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     './app/components/**/*.html.*',
     './app/components/**/*.rb',
   ],
+  theme: {
+    fontFamily: {
+      sans: ['Lato', 'sans-serif']
+    }
+  },
   plugins: [
     require('daisyui')
   ],
