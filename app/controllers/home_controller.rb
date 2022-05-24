729c91fc8cf4d09502311c485d76261f38d979ae
mtayllan <messiastayllan@gmail.com>
2022-05-24 19:18:14 -0300

    List repeats from current and other days

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < AuthenticatedController
     @tasks = current_user.tasks.pending_or_ended_today
     @backlog_tasks = current_user.tasks.in_backlog
     @events = current_user.events.not_done.order(datetime: :asc)
-    @repeats = current_user.repeats.where("days @> ARRAY[?]::integer[]", [Date.today.wday])
+    @today_repeats = current_user.repeats.where("days @> ARRAY[?]::integer[]", [Date.today.wday])
+    @other_repeats = @today_repeats.invert_where
 
     @task = Task.new
     @backlog_task = Task.new
