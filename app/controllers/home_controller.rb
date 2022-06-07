2ac0d31c1ff92d56713d56118cdacaff2003801c
mtayllan <messiastayllan@gmail.com>
2022-06-06 21:30:18 -0300

    Fix timezone issue in repeats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < AuthenticatedController
     @tasks = current_user.tasks.pending_or_ended_today
     @backlog_tasks = current_user.tasks.in_backlog
     @events = current_user.events.not_done.order(datetime: :asc)
-    @today_repeats = current_user.repeats.where("days @> ARRAY[?]::integer[]", [Date.today.wday])
-    @other_repeats = current_user.repeats.where.not("days @> ARRAY[?]::integer[]", [Date.today.wday])
+    @today_repeats = current_user.repeats.where("days @> ARRAY[?]::integer[]", [Time.zone.today.wday])
+    @other_repeats = current_user.repeats.where.not("days @> ARRAY[?]::integer[]", [Time.zone.today.wday])
 
     @task = Task.new
     @backlog_task = Task.new
